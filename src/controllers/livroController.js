5ea1779403dd2d16d04f4235f7b78a1615f1c9d1
Paulo Amaral <pauloamaral2006@yahoo.com.br>
2026-04-09 19:44:53 -0300

    Aula 05

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,6 @@
 import { stat } from "node:fs";
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
   static async listarLivros(req, res) {
@@ -25,9 +26,14 @@ class LivroController {
   }
 
   static async cadastrarLivro(req, res) {
+    const novoLivro = req.body;
     try {
-      const novoLivro = await livro.create(req.body);
-      res.status(201).json({ message: "Livro cadastrado com sucesso" });
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = { ...novoLivro, autor: autorEncontrado };
+      const livroCriado = await livro.create(livroCompleto);
+      res
+        .status(201)
+        .json({ message: "Livro cadastrado com sucesso", livro: livroCriado });
     } catch (error) {
       res
         .status(500)
@@ -36,11 +42,21 @@ class LivroController {
   }
 
   static async atualizarLivro(req, res) {
+    const id = req.params.id;
+    const livroAtualizar = req.body;
+
     try {
-      const id = req.params.id;
-      const livroAtualizado = await livro.findByIdAndUpdate(id, req.body);
+      let livroCompleto = { ...livroAtualizar };
+
+      if (livroAtualizar.autor) {
+        const autorEncontrado = await autor.findById(livroAtualizar.autor);
+
+        livroCompleto = { ...livroCompleto, autor: autorEncontrado };
+      }
+      const livroAtualizado = await livro.findByIdAndUpdate(id, livroCompleto);
       res.status(200).json({
         message: "Livro atualizado com sucesso",
+        livro: livroAtualizado,
       });
     } catch (error) {
       res
@@ -60,6 +76,18 @@ class LivroController {
         .json({ message: `${error.message} - falha ao excluir livro` });
     }
   }
+
+  static async listarLivroPorEditora(req, res) {
+    try {
+      const editora = req.query.editora;
+      const livrosEncontrados = await livro.find({ editora: editora });
+      res.status(200).json(livrosEncontrados);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha ao listar livro por editora` });
+    }
+  } 
 }
 
 export default LivroController;
--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -4,6 +4,7 @@ import LivroController from "../controllers/livroController.js";
 const router = express.Router();
 
 router.get("/livros", LivroController.listarLivros);
+router.get("/livros/busca", LivroController.listarLivroPorEditora);
 router.get("/livros/:id", LivroController.listarLivroPorId);
 router.post("/livros", LivroController.cadastrarLivro);
 router.put("/livros/:id", LivroController.atualizarLivro);
